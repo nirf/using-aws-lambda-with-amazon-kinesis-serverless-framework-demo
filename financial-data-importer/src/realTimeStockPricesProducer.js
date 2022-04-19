adde3b87d41ece960a72084639712dc90ec285f6
nir <nir.feigelshtein@forcepoint.com>
2022-04-19 13:49:03 +0300

    added event source mapping params and implemented a stock price producer script

---
+++ financial-data-importer/src/realTimeStockPricesProducer.js
@@ -0,0 +1,29 @@
+import AWS from 'aws-sdk'
+
+const kinesis = new AWS.Kinesis({apiVersion: '2013-12-03', region: 'eu-west-1'})
+
+function publish(totalPublishTimeInSeconds, publishIntervalInMs) {
+    setInterval(async () => {
+        const result = await kinesis.putRecords({
+            StreamName: 'demoStream',
+            Records: [
+                {
+                    Data: JSON.stringify({symbol: 'AMZN', price: 3055.70, date: new Date().toISOString()}),
+                    PartitionKey: 'AMZN'
+                },
+                {
+                    Data: JSON.stringify({symbol: 'GOOG', price: 2559.22, date: new Date().toISOString()}),
+                    PartitionKey: 'GOOG'
+                }
+            ]
+        }).promise()
+        console.log('Published Records successfully', result)
+    }, publishIntervalInMs)
+
+    setTimeout(() => {
+        console.log('Bye Bye')
+        process.exit()
+    }, totalPublishTimeInSeconds * 1000)
+}
+
+publish(1, 100)
